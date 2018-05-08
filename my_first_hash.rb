1de36bb65d6ee046d421a2c25c1e41337e41df2f
Tony Ho <ho.tonym@gmail.com>
2018-05-08 16:13:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,8 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 shipping_manifest["oil paintings"]
@@ -23,18 +23,19 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
   # add 4 gun powder to the shipping_manifest hash below
 
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
